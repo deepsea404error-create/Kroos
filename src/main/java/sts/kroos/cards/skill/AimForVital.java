0ac1cd7c0b66eb6cc1dd49f21adb8dc0b58141f8
deepsea404error-create <deepsea404error-create@users.noreply.github.com>
2026-08-06 16:48:50 +0800

    要害瞄准改为2点临时力量+文档改名克洛丝.md+README链接修正

--- src/main/java/sts/kroos/cards/skill/AimForVital.java
+++ src/main/java/sts/kroos/cards/skill/AimForVital.java
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import sts.kroos.KroosMod;
 import sts.kroos.cards.AbstractKroosCard;
@@ -11,7 +12,7 @@ import sts.kroos.powers.FlawPower;
 import sts.kroos.powers.FrostPower;
 
 /**
- * 要害瞄准 - 1费, 对敌人施加 1 层破绽; 获得 1 点力量, 1 层寒芒, 抽 1 张牌。
+ * 要害瞄准 - 1费, 对敌人施加 1 层破绽; 获得 2 点临时力量, 1 层寒芒, 抽 1 张牌。
  *   - 强化: 2 层破绽, 2 层寒芒, 抽 2 张牌 (力量不变 1)
  */
 public class AimForVital extends AbstractKroosCard {
@@ -25,7 +26,7 @@ public class AimForVital extends AbstractKroosCard {
     private static final int UPGRADE_FROST = 1;
     private static final int DRAW = 1;
     private static final int UPGRADE_DRAW = 1;
-    private static final int STRENGTH = 1;
+    private static final int STRENGTH = 2;
 
     public AimForVital() {
         super(ID, IMG, COST, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
@@ -37,6 +38,7 @@ public class AimForVital extends AbstractKroosCard {
     public void useImpl(AbstractPlayer p, AbstractMonster m) {
         addToBot(new ApplyPowerAction(m, p, new FlawPower(m, this.magicNumber), this.magicNumber));
         addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, STRENGTH), STRENGTH));
+        addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p, STRENGTH), STRENGTH));
         int frost = this.upgraded ? (FROST + UPGRADE_FROST) : FROST;
         addToBot(new ApplyPowerAction(p, p, new FrostPower(p, frost), frost));
         int draw = this.upgraded ? (DRAW + UPGRADE_DRAW) : DRAW;
